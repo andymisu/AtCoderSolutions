9bc007e7a04f8f2bc1ba76f7381e4be94c457a66
andymisu <misu.martinas@gmail.com>
2024-07-14 14:53:44 +0300

    Create A.cpp

---
+++ AtCoder Beginner Contest 350/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    int n;
+    cin>>c>>c>>c;
+    cin>>n;
+    if(n > 0 && n < 350 && n != 316){
+        cout<<"Yes";
+    }else{
+        cout<<"No";
+    }
+    return 0;
+}
