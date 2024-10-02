63ed4269955aea87c6d135420460d0f64e477e74
andymisu <misu.martinas@gmail.com>
2024-10-02 15:42:28 +0300

    Create A.cpp

---
+++ AtCoder Beginner Contest 358/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s,t;
+    cin>>s;
+    cin>>t;
+   if(s == "AtCoder" && t == "Land"){
+       cout<<"Yes";
+   }else{
+       cout<<"No";
+   }
+    return 0;
+}
